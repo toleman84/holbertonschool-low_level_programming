ebb32c73c3af26515e09d41d5630d5076de8122e
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-27 09:33:55 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc puts ("\"Programming is like building a multilingual puzzle"); return (0);
+#include <stdio.h>
+int main()
+{
+puts ("\"Programming is like building a multilingual puzzle");
+
+return 0;
+}
