84c8f84b906c6e6a7099b33d971d12bc99cdb268
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-27 09:48:53 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,12 +4,10 @@
  * Comment
  * Betty
  * style
- */ 
+ */
 
 int main(void)
 {
-    puts ("\"Programming is like building a multilingual puzzle");
-
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
-
