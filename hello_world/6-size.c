8feb076e13937d8c1aada829c268e106cee5ec97
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-27 11:03:38 -0700

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char charType;
+  int intType;
+  int longIntType;
+  int longLongType;
+  float floatType;
+
+printf("Size of char: %zu bytes\n", sizof(charType));
+printf("Size of int: %zu bytes\n", sizof(charType));
+printf("Size of int: %zu bytes\n", sizof(longIntType));
+printf("Size of int: %u bytes\n", sizof(longLongType));
+printf("Size of float: %zu bytes\n", sizof(floatType));
+return (0);
+}
