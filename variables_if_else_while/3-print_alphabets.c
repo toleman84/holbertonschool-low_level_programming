798a1c4f57fcf883df424144fc22ac8f3a2f8ac5
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-28 20:10:41 -0700

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+char i;
+
+while (i= 'a'; i<= 'z')
+putchar(ch);
+i++;
+
+while (i= 'A'; i<= 'Z')
+putchar(ch);
+i++;
+
+putchar('\n');
+
+return (0);
+}
