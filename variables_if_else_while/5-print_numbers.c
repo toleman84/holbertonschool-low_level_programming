fb48d4aa8d7c39687a2af0c25aeb9f9d134755a3
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 06:47:59 -0700

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+char ch = '0';
+
+while (ch <= '9')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
