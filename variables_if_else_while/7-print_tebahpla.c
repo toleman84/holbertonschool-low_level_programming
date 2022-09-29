653d53c0be88c6abceeced0482cf68d88dede2e9
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 07:07:23 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+char ch = 'z';
+
+while (ch <= 'a')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
