f625c3673d7e30c6035c368d09c49d1b76ff6652
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 07:29:09 -0700

    task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,10 +9,16 @@
 int main(void)
 {
 
-int num;
+int i;
 
-scanf("%x", &num);
-putchar("%x %d", num, num);
+for (i = 0; i <= 10; i++)
+{
+putchar(i);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
 
 putchar('\n');
 return (0);
