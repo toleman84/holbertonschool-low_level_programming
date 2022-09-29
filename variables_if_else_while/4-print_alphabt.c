8c3cc9f1f3f41c9669779ffd8170f6ee48e059ea
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 06:18:25 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+char abc;
+
+for (abc = 'a'; abc <= 'd'; abc = 'f'; abc <= 'p'; abc = 'r'; abc <= 'z'; abc++)
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
+
