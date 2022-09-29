6db46df03586dd14d73f863c9320d760f1921667
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 05:47:10 -0700

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,12 +13,14 @@ int main(void)
 
 char abc;
 
-while (abc = 'a'; abc <= 'z'; abc++)
+while (abc='a'; abc<='z'; abc++)
+{
 putchar(abc);
-
-while (abc = 'A'; abc <= 'Z'; abc++)
+}
+while (abc='A'; abc<='Z'; abc++)
+{
 putchar(abc);
-
+}
 putchar('\n');
 return (0);
 }
