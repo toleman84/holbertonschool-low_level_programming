5bdc4816571ad4d03e62634c0ebfed5ee11a82a0
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-28 19:42:48 -0700

    task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,16 +7,16 @@
  * Return: 0 (Success)
  */
 
-int main() {
+int main(void)
 
 char ch = 'a';
 
-while(ch <= 'z')
+while (ch <= 'z')
 {
 putchar(ch);
 ch++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
 
