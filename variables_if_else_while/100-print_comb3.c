46d1b0b0a9e9f954e015966b9d8d8bd2cf2b75b7
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 10:22:48 -0700

    task 100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -18,12 +18,12 @@ for (num2 = num1 + 1; num2 <= 9; num2++)
 {
 putchar(num1 + '0');
 putchar(num2 + '0');
-if (num1 == '8' && num2 == '8')
+if (num1 == '8' && num2 == '9')
 {
 break;
+}
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
