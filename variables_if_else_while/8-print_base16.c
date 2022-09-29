541af1bcac7bff54f490415830925109f110b10b
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 07:20:57 -0700

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+int num;
+
+scanf("%x", &num);
+putchar("%x %d", num, num);
+
+putchar('\n');
+return (0);
+}
+
