4f5720cd60c03d9a99a753273babddd7ed925934
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-29 06:50:39 -0700

    task 5

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+int num = '0';
+
+for (num <= '9')
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
