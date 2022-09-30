a7cb0cc8dcb669bf37d977c7dc7a42df37a7b95f
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-30 08:41:51 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -12,9 +12,14 @@
 int main(void)
 {
 
-char *ch[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
-
-_putchar(ch);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
