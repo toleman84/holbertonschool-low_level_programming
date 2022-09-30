8623d319665589fce3543cd76db4a3e88be47e2f
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-30 09:24:21 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Short description
+ * 
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char abc;
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+_putchar(abc);
+}
+_putchar('\n')
+return(0);
+}
+
