17ff098b779d3eef53b83940dfa562f04be908d6
toleman84 <gdponcemarsiglia@gmail.com>
2022-09-30 09:55:28 -0700

    task 1

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void);
+{
+
+char abc;
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+for I in 1 .. 10 loop
+_putchar(abc);
+}
+_putchar(\n);
+}
+
