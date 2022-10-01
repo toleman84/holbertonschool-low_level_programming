ffd35b1d104f74d26f4fc2764be6877bcc51dcbd
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-01 16:05:13 -0700

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -13,16 +13,19 @@ int _isalpha(int c)
 char abc;
 char ABC;
 
-for (ABC < 'A'; ABC <= 'Z'; abc++)
+for (ABC < 'A'; ABC <= 'Z'; ABC++)
+{
+if (ABC == c)
+{
+return (1);
+}
+}
+
 for (abc = 'a'; abc <= 'z'; abc++)
 {
 if (abc == c)
 {
 return (1);
-if (ABC == c)
-{
-return (1);
-}
 }
 }
 return (0);
