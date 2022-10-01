a56dbe208e16082df0a0f8aebc8b5e467aec007e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-01 10:52:12 -0700

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _islower - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int _islower (int c);
+
+char abc;
+int true = 1;
+int false = 0;
+
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+if (islower(c))
+{
+_putchar(true);
+}
+else
+{
+_putchar(false);
+}
+return (0);
+}
