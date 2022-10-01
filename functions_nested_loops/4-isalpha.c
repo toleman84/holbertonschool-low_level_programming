9decdcd282e86602b9ff7ae27892130fabb76089
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-01 13:52:31 -0700

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Short description
+ *
+ * Return: 0 (Success)
+ * @c: Variable
+ */
+
+int _isalpha(int c)
+{
+
+int letter;
+
+if (_isalpha(letter))
+{_putchar('1');}
+
+else{_putchar('0');}
+
+return (0);
+}
+
