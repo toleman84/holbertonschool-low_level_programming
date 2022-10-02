79c2a48312b8dae77fba847d9e53df0ae0392b6e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 10:50:01 -0700

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Short description
+ *
+ * Description: function prints 9 times table
+ * Return: 0 (Success)
+ */
+
+void times_table(void)
+{
+
+int mdr;
+int mnd;
+
+for (mdr = 0; mdr < 10; mdr++)
+{
+{
+for (mnd = 0; mnd < 10; mnd++)
+}
+
+_putchar(mdr * mnd);
+}
+
+return(0);
+}
+
