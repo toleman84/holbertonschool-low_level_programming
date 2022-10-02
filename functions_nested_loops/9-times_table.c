3cf4254da97b3cb45e7b97868ef8ebde0abb0487
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 15:05:55 -0700

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ for (mdr = 0; mdr < 10; mdr++)
 	n = mdr * mnd;
 	if ((n / 10) == 0)
 	{
-        _putchar(' '):
+        _putchar(' ');
 	_putchar((n / 10) + '0');
 	_putchar(',');
 	_putchar(' ');
