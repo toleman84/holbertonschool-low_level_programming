a23c33d276be6d6f20a7cefdff749bc0d38112e4
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-01 17:37:43 -0700

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,11 +13,12 @@ int print_sign(int n)
 int num;
 
 for (num = 0; num <= 9; num++)
-if (n == num) {return (1); _putchar('+')}
+{
+if (n == num) {return (1); _putchar('+');}
 
-else {return (0); _putchar('0')}
+else {return (0); _putchar('0');}
 
-if (num == n) {return (1-2); _putchar('-')}
-
+if (num == n) {return (1-2); _putchar('-');}
+}
 }
 
