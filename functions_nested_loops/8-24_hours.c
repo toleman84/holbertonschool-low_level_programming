4240aaf8b760a6fa0e394f503f9a8d12d64c5e67
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 09:42:19 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_buaer - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+
+int h;
+int m;
+
+for (h = 00; h <= 23; h++)
+{
+for (m = 00; h <= 59; m++)
+}
+
+_putchar(h);
+_putchar(':');
+_putchar(m);
+_putchar('\n');
+
+return(0);
+}
