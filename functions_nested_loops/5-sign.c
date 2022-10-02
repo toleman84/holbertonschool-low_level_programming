9bfe72996c6398f9bc30bd599561c85cd5f7d728
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-01 17:35:14 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Short description
+ *
+ * Return: 0 (Success)
+ * @c: Variable
+ */
+
+int print_sign(int n)
+{
+
+int num;
+
+for (num = 0; num <= 9; num++)
+if (n == num) {return (1); _putchar('+')}
+
+else {return (0); _putchar('0')}
+
+if (num == n) {return (1-2); _putchar('-')}
+
+}
+
