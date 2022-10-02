b709d73424029d27f0be624549f55e7877c75378
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 10:08:26 -0700

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,16 +12,15 @@ void jack_bauer(void)
 int h;
 int m;
 
-for (h = 0; h <= 23; h++)
+for (h = 0; h < 24; h++)
 {
-for (m = 0; h <= 59; m++)
+for (m = 0; m < 60; m++)
 {
-
 _putchar((h / 10) + '0');
-_putchar((h % 10) + '0');
+_putchar(10 + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
-_putchar((m % 10) + '0');
+_putchar(m + '0');
 _putchar('\n');
 }
 }
