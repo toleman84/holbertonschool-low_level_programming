155dfd6f5faf9a9f46b7a2249f0b76fa13452fd0
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 10:10:38 -0700

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -17,10 +17,10 @@ for (h = 0; h < 24; h++)
 for (m = 0; m < 60; m++)
 {
 _putchar((h / 10) + '0');
-_putchar(10 + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
-_putchar(m + '0');
+_putchar((m % 10) + '0');
 _putchar('\n');
 }
 }
