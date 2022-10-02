ac3e0b4b1074d23f56286e4ecbf48c3586f7f5b7
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-01 19:47:15 -0700

    task 6

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Short description
+ *
+ * Return: 0 (Success)
+ * @r: Variable
+ */
+
+int print_last_digit(int r)
+{
+return (lastDigit = r % 10);
+}
+
