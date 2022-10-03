d711113927dace9dd88b6910a6b81a62fb0f0b9e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 17:29:42 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - Short description
+ * @num1: integer num1
+ * @num2: integer num2
+ * Description: 
+ * Return: 0 (Success)
+ */
+
+int add(int num1, int num2)
+{
+
+int num1;
+int num2;
+int sum;
+
+sum = num1 + sum2;
+
+return (0);
+}
+
