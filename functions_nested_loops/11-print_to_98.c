b3340e70989c6470622d6a2ae5c63e1d8b1e57b1
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 17:50:51 -0700

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+
+
+void print_to_98(int n)
+{
+
+for (; n <=98; n++)
+{
+_putchar(n);
+}
+
+for (; n >=98; n--)
+{
+_putchar(n);
+}
+
+_putchar('\n');
+}
