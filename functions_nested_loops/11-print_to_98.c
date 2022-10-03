184b3271e4d0778177a669c4fc33972542931ab0
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 19:14:04 -0700

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,27 +10,32 @@
 
 void print_to_98(int n)
 {
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			_putchar(n);
 
-if (n <= 98)
-{
-	for (; n <= 98; n++)
-		{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+			if (n == 98)
+			{
+			_putchar(", ");
+			}
+		_putchar("\n");
 		}
-	_putchar('\n');
-}
-else
-{
-	for (; n >= 98; n--)
+	}
+	else
+	{
+		for (; n >= 98; n--)
 		{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+		_putchar("%d", n);
+
+		if (n == 98)
+		{
+			_putchar(", ");
 		}
-	_putchar('\n');
-}
+	_putchar("\n");
+		}
+	}
 
 }
 
