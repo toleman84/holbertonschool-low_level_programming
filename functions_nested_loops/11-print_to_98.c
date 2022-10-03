01c7f631ed1df1e269d24586b365ac0f1d8ac92d
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-02 17:53:34 -0700

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 
-
-
+/**
+ * print_to_98 - Short description
+ *
+ * Return: 0 (Success)
+ * @n: Variable
+ */
 
 void print_to_98(int n)
 {
 
-for (; n <=98; n++)
+for (; n <= 98; n++)
 {
 _putchar(n);
 }
 
-for (; n >=98; n--)
+for (; n >= 98; n--)
 {
 _putchar(n);
 }
