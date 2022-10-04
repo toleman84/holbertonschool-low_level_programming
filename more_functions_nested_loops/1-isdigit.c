03a727c0c7c4b26fbb9bb03152353b7720160ffd
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 07:13:59 -0700

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isdigit - Short description
+ *
+ * Return: Always 0 (Success)
+ * @c: Variable
+ */
+
+int _isdigit(int c)
+{
+
+int num;
+
+for (num = '0'; num <= '9'; num++)
+{
+{
+if (c == num)
+}
+return (1);
+}
+
+return (0);
+}
+
