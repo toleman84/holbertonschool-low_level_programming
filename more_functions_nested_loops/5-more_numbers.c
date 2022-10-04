88699cf42f6db22f2e7069a2e7d9e9822b6e70a3
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 09:29:24 -0700

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Short description
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+
+int num;
+int nums;
+
+for (num = 1; num <= 10; num++)
+{
+for (nums = 0; nums <= 14; nums++)
+{
+_putchar(nums);
+}
+_putchar('\n');
+}
+}
+
