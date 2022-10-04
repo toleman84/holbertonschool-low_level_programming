4034a8dd0d1adbe78d62299deff0dbe275feff67
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 08:05:48 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+void print_numbers(void)
+{
+
+int num;
+
+for (numm = '0'; num <= '9'; num++)
+_putchar(num);
+_putchar('\n');
+}
+
