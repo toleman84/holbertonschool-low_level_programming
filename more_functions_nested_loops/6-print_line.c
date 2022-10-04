8cef4fac72c1c8fa9d60a38502566e0963a80493
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 09:48:51 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Short description
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+
+char ch;
+
+for (; ch <= n; ch++)
+{
+_putchar('_');
+_putchar('\n');
+
+if (n == 0)
+{
+_putchar('\n');
+}
+}
+
+}
+
