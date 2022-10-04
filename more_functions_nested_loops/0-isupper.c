50594c649ba3d20366c626ab57b86e63c15e98ea
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 06:47:07 -0700

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Short description
+ *
+ * Return: Always 0 (Success)
+ */
+
+nt _isupper(int c)
+{
+
+char c;
+
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else {
+return (0);
+}
+
+}
+
