29b5ac70cc808097d986dab81b30abdeaf5dd02e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 08:42:27 -0700

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+
+int num;
+
+for (num = '0'; num <= '9'; num++)
+{
+if (num != '2' && num != '4')
+{
+_putchar(num);
+}
+}
+
+}
