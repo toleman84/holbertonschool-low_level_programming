751d7d3ac4310c1ed10e574b0d058c8943b765b9
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 17:58:07 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * print_diagonal - Short description
- * @n: Variable
+ * main - Short description
+ *
  * Return: Always 0 (Success)
  */
 
@@ -10,9 +10,9 @@ int main(){
 
 int num;
 
-for (; num <= 100; num++){
+for (num = 1; num <= 100; num++){
 
-if (num % 3 == 0 && num % 5 == 0){printf("FizzBuzz ");}
+if ((num % 3) == 0 && (num % 5) == 0){printf("FizzBuzz ");}
 
 else if (num % 3 == 0) {printf("Fizz ");}
 
