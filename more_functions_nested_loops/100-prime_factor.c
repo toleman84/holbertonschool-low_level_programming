d2be3b0b4ce3bff9660ac4e0e0684a9ef6b7dc98
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-05 13:03:10 -0700

    task 100

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Short description
+ *
+ * Return: 0 (Success)
+ */
+
+int main ()
+{
+
+long int n = 612852475143;
+int n2, n3;
+
+if (n % 2 == 0) { printf("2"); }
+else if (n % 3 == 0) { printf("3"); }
+else if (n % 5 == 0) { printf("5"); }
+
+return (0);
+}
