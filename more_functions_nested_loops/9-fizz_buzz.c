bac001315e758060bff0b3409db36337e4637146
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 18:09:01 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ if (num % 3 == 0 && num % 5 == 0){printf("FizzBuzz ");}
 
 else if (num % 3 == 0) {printf("Fizz ");}
 
-else if (num % 5 == 0) {printf("Buzz ");}
-
 else if (num == 100) {printf("Buzz");}
+
+else if (num % 5 == 0) {printf("Buzz ");}
 
 else {printf("%d ", num);}
 
