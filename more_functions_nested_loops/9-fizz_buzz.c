77522b20603a6a413502bf211d662a241c60bde8
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 17:43:33 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int num;
 
 for (num = 1; num <= 100; num++){
 
-if (num % 3 == 0) {printf("Fizz");}
+if (num % 3 == 0) {printf("Fizz ");}
 
-else if (num % 5 == 0){printf("Buzz");}
+else if (num % 5 == 0){printf("Buzz ");}
 
-else if (num % 3 == 0 && num % 5 == 0){printf("FizzBuzz");}
+else if (num % 3 == 0 && num % 5 == 0){printf("FizzBuzz ");}
 
-else {printf("%d", num);}
+else {printf("%d ", num);}
 
 }
 
