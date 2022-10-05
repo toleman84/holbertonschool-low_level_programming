523a386653ed2e4eada8f178365f4587b0b64f1e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-04 19:05:10 -0700

    task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,9 +15,9 @@ int n = size - 1;
 
 if (size > 0)
 {
-for (i = 0; i <= size; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; j <= size; j++)
+for (j = 0; j < size; j++)
 {
 if (j < n)
 {
@@ -27,14 +27,16 @@ else
 {
 _putchar('#');
 }
+}
 n--;
 _putchar('\n');
-}
+
 }
 }
 else
 {
 _putchar('\n');
 }
+
 }
 
