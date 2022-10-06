b37e567ec87a36dfc6199e1d8969f712e690d21b
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-06 10:30:15 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Short description
+ *
+ * Return: Always 0 (Success)
+ * @c: Variable
+ */
+
+void swap_int(int *a, int *b)
+{
+
+swap (int *a, int *b);
+
+}
