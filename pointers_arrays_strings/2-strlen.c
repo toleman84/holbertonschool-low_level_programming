9d98e16148432f0ce4f61a72a362545f8aa6634f
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-06 10:50:17 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Short description
+ *
+ * Return: Always 0 (Success)
+ * @s: Variable
+ */
+
+int _strlen(char *s)
+{
+
+size_t strlen(*s);
+
+
+
+}
