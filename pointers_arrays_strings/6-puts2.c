a45bbb3edd1a97b35aa7a2530ee4840f930fd518
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-07 08:53:02 -0700

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,14 @@
 void puts2(char *str)
 {
 
-int i = 0;
-int len = strlen(str);
+int i;
 
-for (; i < len; i += 0)
+for (i = 0; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 
 }
+_putchar('\n');
 
 }
+
