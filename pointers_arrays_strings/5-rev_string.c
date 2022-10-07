97f50dd9a6d1a73f6bb57f100be2bfdd226c4215
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-07 06:15:52 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Short descriptcion
+ *
+ * Return: Always 0 (Success)
+ * @s : Variable
+ */
+
+void rev_string(char *s)
+{
+
+int i;
+int temp;
+
+
+for (i = 0; s[i] != '\0'; i++)
+{
+temp = s[i];
+s[i] = s[i - 1];
+s[i - 1] = temp;
+
+}
+
+}
+
