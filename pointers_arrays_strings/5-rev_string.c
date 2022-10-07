b3a6cb0d84d767797dc3e2cebb3ed10b19bdebb3
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-07 06:45:30 -0700

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -15,17 +15,14 @@ int i;
 int len;
 char temp;
 
+len = strlen(s);
 
-
-for (i = 0; s[len] != '\0'; len++)
-{
-for (; i < len-- ;)
+for (i = 0; s[i] != '\0'; i++)
 {
 temp = s[i];
-s[i++] = s[len];
-s[len] = temp;
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
 
-}
 }
 
 }
