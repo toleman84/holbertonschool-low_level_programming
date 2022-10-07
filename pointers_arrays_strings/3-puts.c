0f015fc2b9580b140c4d93a756e6af00f3a76871
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-06 18:53:32 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdout.h>
+
+/**
+ * _puts - Short description
+ *
+ * Return: Always 0 (Success)
+ * @str: Variable
+ */
+
+void _puts(char *str)
+{
+
+int _puts(const char *str);
+
+
+}
