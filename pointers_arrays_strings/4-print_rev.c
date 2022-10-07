34be2a79f9a34af76886e8c3347f224c4694e094
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-06 19:43:27 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Short description
+ *
+ * Return: Always 0 (Success)
+ * @str: Variable
+ */
+
+void print_rev(char *s)
+{
+
+int i;
+
+for (str[i] != '\0'; i = 0; i++)
+{
+putchar(str[i]);
+
+}
+putchar('\0');
+
+}
+
