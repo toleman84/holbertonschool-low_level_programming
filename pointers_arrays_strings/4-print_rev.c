7e4420f46cc95078c794e01711c908a86da5709f
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-07 08:16:23 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,18 +12,11 @@ void print_rev(char *s)
 {
 
 int i;
-int len;
-int temp;
-
-len = strlen(s);
 
 for (i = 0; s[i] != '\0'; i++)
 {
-temp = s[i];
-s[i] = s[len - i - 1];
-s[len - i - 1] = temp;
-
-_putchar(s[i]);
+for (i)
+_putchar(s[--i]);
 
 }
 _putchar('\n');
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -17,13 +17,12 @@ char temp;
 
 len = strlen(s);
 
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; i < len/2; i++)
 {
 temp = s[i];
 s[i] = s[len - i - 1];
 s[len - i - 1] = temp;
 
-_putchar(s[i]);
 }
 _putchar('\n');
 
