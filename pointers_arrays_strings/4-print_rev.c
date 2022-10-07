e935a4f6ab95013894ebd99ed37e8a09de55c87d
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-07 05:54:36 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,19 +4,24 @@
  * print_rev - Short description
  *
  * Return: Always 0 (Success)
- * @str: Variable
+ * @s: Variable
  */
 
 void print_rev(char *s)
 {
 
 int i;
-int n = *s - 1;
+int len;
+int temp;
 
-for (i = 0; i < n; i++)
+len = strlen(s);
+
+for (i = 0; i < len/2; i++)
 {
-i--;
-_putchar(s[i]);
+
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
 
 }
 _putchar('\n');
