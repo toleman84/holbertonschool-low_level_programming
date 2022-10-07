bdd543f93dc0c869035980753a938f1912921314
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-07 06:06:11 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -15,16 +15,15 @@ int i;
 int len;
 int temp;
 
-len = strlen(s);
 
 for (i = 0; s[i] != '\0'; i++)
 {
 
 temp = s[i];
-s[i] = s[len - i - 1];
-s[len - i - 1] = temp;
+s[i] = s[i - 1];
+s[i - 1] = temp;
 
-_putchar(s[i]);
+_putchar(s[i - 1]);
 
 }
 _putchar('\n');
