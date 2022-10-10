ad9a29623f9ba9da37f47fbee0757a28334f441a
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-09 22:04:50 -0700

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,15 +14,14 @@ void print_array(int *a, int n)
 
 int i;
 
-for (i = 0; i < n; i++)
-{
+for (i = 0; i < n; i++){
 printf("%d", a[i]);
 if (i < n - 1)
 {
 printf(", ");
 }
 }
-_putchar('\n');
+
 
 }
 
