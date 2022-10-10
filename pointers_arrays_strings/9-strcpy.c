1a3718042522f38e1e9d578d4cda95670006682a
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-10 12:10:35 -0700

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Short description
+ *
+ * Return: 0 (Success)
+ * @r: Variable
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+while (scr)
+{
+dest = src;
+src++;
+dest++;
+}
+
+return (dest);
+}
