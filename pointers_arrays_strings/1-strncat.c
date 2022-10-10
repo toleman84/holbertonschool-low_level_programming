b2ba4387f59ee6c0d1a0b16dd6a2c10efbbc8cae
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-10 16:18:09 -0700

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - Short description
+ *
+ * Return: 0 (Success)
+ * @dest: Variable
+ * @src: Variable
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int j;
+int lengh = 0;
+
+while (dest[lengh] != '\0')
+{
+++lengh;
+}
+
+for (j = 0; src[j] != '\0'; ++j, ++lengh)
+{
+dest[lengh] = src[j];
+}
+
+for (j = 0; j < n; j++)
+{
+}
+
+if (src[j] < n){dest[lengh] = '\0';}
+
+return (dest);
+}
