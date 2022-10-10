10126812924564883821243003fea35de47405ad
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-10 16:54:54 -0700

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,10 +15,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 size_t i;
 
-for (i = 0; i < n && src[i] != '\0'; i++){
-dest[i] = src[i];}
-for ( ; i < n; i++){
-dest[i] = '\0';}
+for (i = 0; i < n; i++)
+
+while (src[i] != '\0') {dest[i] = src[i];}
+
+for (; i < n; i++) {dest[i] = '\0';}
 
 return (dest);
 }
