3df45f487eeb4bdcc405efb134e0c9f5a952edf2
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-10 15:39:10 -0700

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Short description
+ * strcat - Short description
  *
  * Return: 0 (Success)
- * @n: Variable
+ * @dest: Variable
+ * @src: Variable
  */
 
 char *_strcat(char *dest, char *src)
@@ -13,9 +14,15 @@ char *_strcat(char *dest, char *src)
 int j;
 int lengh = 0;
 
-while (dest[lengh] != '\0'){++lengh;}
+while (dest[lengh] != '\0')
+{
+++lengh;
+}
 
-for (j = 0; src[j] != '\0'; ++j, ++lengh) {dest[lengh] = src[j];}
+for (j = 0; src[j] != '\0'; ++j, ++lengh)
+{
+dest[lengh] = src[j];
+}
 
 dest[lengh] = '\0';
 
