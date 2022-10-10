b4a287570a97bfc01b1944c8816f7ea486426202
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-09 20:11:52 -0700

    task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -21,14 +21,11 @@ for (lengh = 0; str[lengh] != '\0'; lengh++)
 if (lengh % 2 != 0)
 {
 n = (lengh - 1) / 2;
-_putchar(str[lengh]);
-}
-
 for (i = n; i < lengh; i++)
 {
 _putchar(str[i]);
 }
-
+}
 }
 _putchar('\n');
 
