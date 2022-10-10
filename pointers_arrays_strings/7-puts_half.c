78ca4e97d5b1aa0f03e5f2d0c1f8a5aaa8e69662
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-09 21:08:02 -0700

    task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,8 @@ void puts_half(char *str)
 int i, k, n;
 
 for (i = 0; str[i] != '\0'; i++){
-if (i % 2 != 0){n = (i - 1) / 2;}
-else {n = i / 2;}
+if (i % 2 == 0){n = (i / 2;}
+else {n = (i + 1) / 2;}
 for (k = n; k <= str[i]; k++)
 {
 _putchar(str[i]);
