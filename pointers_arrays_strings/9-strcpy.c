bbd596079e8fd34fb8746b347f5e4094c4c5b40e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-10 14:44:00 -0700

    task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,11 @@ char *_strcpy(char *dest, char *src)
 
 int i = 0;
 
-for (; (dest[i] != '\0'; i++)
+for (; (dest[i] != '\0')
 
-for (; src[i] != '\0'; i++)
+for (; src[i] != '\0')
+
+i++;
 
 dest[i] = src[i];
 
