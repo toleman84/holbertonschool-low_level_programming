15deda988ca6c9cb9bf964f8f5f0477f9afa005c
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-09 21:56:20 -0700

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * swap_int - Short description
+ *
+ * Return: Always 0 (Success)
+ * @a: Variable
+ * @b: Variable
+ */
+
+void print_array(int *a, int n)
+{
+
+int i;
+int lengh;
+
+for (lengh = 0; a[lengh] != '\0'; lengh++)
+{
+
+}
+for (i = n; i <= lengh; i++)
+{
+_putchar(a[i]);
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+
+}
+
