b0677e3fc8050d137038fad6408283813edb2197
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-11 08:41:17 -0700

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - Short description
+ *
+ * Return: Always 0 (Success)
+ * @str: Variable
+ */
+
+char *cap_string(char *str)
+{
+
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 'a' + 'A';
+}
+
+if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+{
+str[i] = str[i] - 'a' + 'A';
+}
+
+i++;
+}
+
+return (str);
+}
+
