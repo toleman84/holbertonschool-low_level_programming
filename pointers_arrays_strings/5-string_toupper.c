d4c9a78c8de2e802446d324d367efa50c019e999
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-11 07:48:13 -0700

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Short description
+ *
+ * Return: Always 0 (Success)
+ * @str: Variable
+ */
+
+char *string_toupper(char *str)
+{
+
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+
+}
+
+return (str);
+}
