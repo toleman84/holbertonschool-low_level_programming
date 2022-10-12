cfa3dce5585adf2fba611ba1e5cb755f1acd281b
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-12 07:21:02 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,13 +17,24 @@ for (i = 0; str[i] != '\0'; i++)
 if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
 {
 str[i] = str[i] - 'a' + 'A';
+}
 
-if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}'))
+while (str[i] != '\0')
 {
-str[i] = str[i] - 32;
+if ((str[i] >= 'a' && str[i] <= 'z') && 
+(str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || 
+str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || 
+str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || 
+str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || 
+str[i - 1] == '}'))
+{
+str[i] = str[i] - 'a' + 'A';
 }
+i++;
 }
+
 }
+
 return (str);
 }
 
