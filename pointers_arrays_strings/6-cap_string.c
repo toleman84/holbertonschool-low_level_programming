3c54b02041c80093074b184fcd8a24d5f4a33597
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-12 05:46:38 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,20 +16,20 @@ for (i = 0; str[i] != '\0'; i++)
 {
 if (i == 0)
 {
-	if (str[i] >= 'a' && str[i] <= 'z') {str[i] = str[i] - 'a' + 'A';}
+	if (str[i] >= 'a' && str[i] <= 'z') {str[i] = str[i] -= 'a' + 'A';}
 }
 
-if(str[i] == ' ')
+if (str[i] == ' ')
 {
 	i++;
 	if (str[i] >= 'a' && str[i] <= 'z')
 	{
-	str[i] = str[i] - 'a' + 'A';
+	str[i] = str[i] -= 'a' + 'A';
 	}
 }
 else
 {
-        if (str[i] >= 'A' && str[i] <= 'Z'){str[i] = str[i] + 32;}
+        if (str[i] >= 'A' && str[i] <= 'Z'){str[i] = str[i] += 'A' + 'a';}
 }
 }
 
