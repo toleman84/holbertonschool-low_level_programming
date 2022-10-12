bcfb5ee17fd94bdf51f16413c49daa717c2aa549
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-12 05:58:44 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,23 +14,10 @@ int i;
 
 for (i = 0; str[i] != '\0'; i++)
 {
-if (i == 0)
-{
-	if (str[i] >= 'a' && str[i] <= 'z') {str[i] = str[i] - 'a' + 'A';}
-}
 
-if (str[i] == ' ')
-{
-	i++;
-	if (str[i] >= 'a' && str[i] <= 'z')
-	{
-	str[i] = str[i] - 'a' + 'A';
-	}
-}
-else
-{
-        if (str[i] >= 'A' && str[i] <= 'Z'){str[i] = str[i] + 32;}
-}
+
+if (i == 0 && (str[i] >= 'a' && str[i] <= 'z')) {str[i] = str[i] - 'a' + 'A';}
+
 }
 
 while (str[i] != '\0')
