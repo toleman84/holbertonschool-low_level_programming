26e10bf2ae3cbb045abfd1399353bb074c25d55e
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-13 06:23:15 -0700

    task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Short description
+ *
+ * Return: Always 0 (Success)
+ * @s: Variable
+ * @accept: Variable
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+int i;
+int j;
+int count = 0;
+
+for (j = 0; accept[j] != '\0';j++)
+{
+for (i = 0; s[i] != 32 && s[i] != '\0'; i++)
+{
+if (s[i] == accept[j])
+{
+count++;
+}
+else
+{
+return (NULL);
+}
+}
+}
+
+return (0);
+}
+
