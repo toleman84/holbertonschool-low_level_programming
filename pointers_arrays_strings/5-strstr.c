44706bcd79646893d18b74fd9222ae28ab57ffba
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-13 10:19:11 -0700

    task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -20,7 +20,7 @@ for (j = 0; haystack[j] != '\0'; j++)
 {
 for (i = 0; needle[i] != '\0'; i++)
 {
-if (haystack[j] == needle[i]) {return (needle + j);}
+if (haystack[j] == needle[i]) {return (needle);}
 
 }
 }
