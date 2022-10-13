f22039f494aa6fb05923280e96c032df1f8f82dc
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-13 06:34:38 -0700

    task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -15,7 +15,6 @@ char *_strpbrk(char *s, char *accept)
 
 int i;
 int j;
-int count = 0;
 
 for (j = 0; accept[j] != '\0'; j++)
 {
@@ -23,7 +22,8 @@ for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == accept[j])
 {
-count++;
+return (s);
+
 }
 
 }
