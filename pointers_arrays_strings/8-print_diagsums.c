b6691087f7eea97cc8af28ae6221c0cd2685c2b7
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-13 08:29:30 -0700

    task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -21,12 +21,13 @@ for (i = 0; i < size; i++)
 sum1 += a[i];
 a += size;
 }
+
 for (i = 0; i < size; i++)
 {
 sum2 += a[i];
-a -= size;
+a += size;
 }
 
-printf("%d, %d\n", sum1, sum2);
+printf("%d, %d \n", sum1, sum2);
 }
 
