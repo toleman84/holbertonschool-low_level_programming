65b01cffacbbba2838088c62a17e5ae81de28bda
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-15 19:40:49 -0700

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * sqrt_rec - short description
+ *
+ * @k: first member
+ * @n: second member
+ * Return: Always 0 (Success)
+ */
+
+int sqrt_rec(int k, int n)
+{
+
+int next_k = (k + n / k) / 2;
+
+if (k * k <= n && (k + 1) * (k + 1) > n)
+{
+return (k);
+}
+
+else
+{
+return (sqrt_rec(next_k, n));
+}
+
+}
+
+
+/**
+ * _sqrt_recursion - short description
+ *
+ * @n: first member
+ * Return: Always 0 (Success)
+ */
+
+int _sqrt_recursion(int n)
+{
+
+if (n < 0)
+{
+return (-1);
+}
+
+else
+{
+return (sqrt_rec(1, n));
+}
+
+}
+
