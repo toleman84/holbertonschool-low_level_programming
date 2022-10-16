b2fd04427d0e15d68c4091e3c17c3ac1c8ec296c
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-15 18:56:21 -0700

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - short description
+ *
+ * @x: first member
+ * @y: second member
+ * Return: Always 0 (Success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+{
+return (-1);
+}
+
+else if (y == 0)
+{
+return (1);
+}
+
+else
+{
+return (x * (_pow_recursion(x, y - 1)));
+}
+
+}
+
