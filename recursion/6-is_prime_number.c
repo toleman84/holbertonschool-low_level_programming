bdbfe09568005b96f053f53ae4de5036f99072f1
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-16 19:29:56 -0700

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,12 +10,12 @@
 int is_prime_number(int n)
 {
 
-if (n / n != 1 && n / 1 != n)
+if (n / n == 1 && n / 1 == n)
 {
 return (1);
 }
 
-else if (n / n == 1 && n / 1 == n)
+else if (n / n != 1 && n / 1 != n)
 {
 return (0);
 }
