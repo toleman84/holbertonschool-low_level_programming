33aef4d6dea14dd7c9ced3843150f672cd750c19
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-17 10:32:27 -0700

    task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,13 +9,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+printf("%d\n", argc);
 return (0);
 }
 
