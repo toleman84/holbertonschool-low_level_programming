a4097b3c5cb4b697d257a252829dcf054b761181
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-17 10:16:31 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Short description
+ *
+ * @argc: first member
+ * @argv: second member
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("argc = %d\n", argc);
+return (0);
+}
+
