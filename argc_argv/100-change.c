60c74c232014cdf6615739d6ddd1bf73c6a66ddf
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-18 08:57:47 -0700

    task 100

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -12,47 +12,46 @@
 
 int main(int argc, char *argv[])
 {
-int cash;
-int coins = 0;
-
+int cash, coins = 0;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
 if (argv[1][0] == '-')
 {
 printf("0\n");
 return (0);
 }
-
 if (argc == 2)
 {
 cash = atoi(argv[1]);
-
-if (cash % 25 >= 0){
-coins += cash / 25;
-cash = cash % 25;}
-
-if (cash % 10 >= 0){
-coins += cash / 10;
-cash = cash % 10;}
-
-if (cash % 5 >= 0){
-coins += cash / 5;
-cash = cash % 5;}
-
-if (cash % 2 >= 0){
-coins += cash / 2;
-cash = cash % 2;}
-
-if (cash % 1 >= 0){
-coins += cash / 1;
-cash = cash % 1;}
-
-printf("%d\n", coins);
 }
+if (cash % 25 >= 0)
+{
+coins += cash / 25;
+cash = cash % 25;
+}
+if (cash % 10 >= 0)
+{
+coins += cash / 10;
+cash = cash % 10;
+}
+if (cash % 5 >= 0)
+{
+coins += cash / 5;
+cash = cash % 5;
+}
+if (cash % 2 >= 0)
+{
+coins += cash / 2;
+cash = cash % 2;
+}
+if (cash % 1 >= 0)
+{
+coins += cash;
+}
+printf("%d\n", coins);
 return (0);
 }
 
