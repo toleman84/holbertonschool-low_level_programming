739c83594062e4a387d263f27577f4ee5248a4af
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-19 08:50:59 -0700

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,5 +13,19 @@
 char *create_array(unsigned int size, char c)
 {
 
+char *ar = malloc(size * sizeof(c));
+printf("%s\n", ar);
 
+if (size == 0)
+{
+return (NULL);
 }
+
+if (!c)
+{
+return (NULL);
+}
+
+return (0);
+}
+
