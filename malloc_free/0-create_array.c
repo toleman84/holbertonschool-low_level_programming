9bcfe4ff2afa1361a1c530de32aa8484bedf3140
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-19 07:17:01 -0700

    new directory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Short description
+ *
+ * @size: first member
+ * @c: second member
+ * Return: Always 0 (Success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+
+}
