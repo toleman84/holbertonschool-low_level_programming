1a3f607d7a9d1272f024de12425381f9a4d4abba
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-22 15:24:08 -0700

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Short description
+ *
+ * @width: first member
+ * @height: second member
+ * Return: Always 0 (Success)
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i;
+int **grid;
+
+if (width < 1)
+{
+return (0);
+}
+
+if (height < 1)
+{
+return (0);
+}
+
+grid = malloc(height * sizeof(int *));
+
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+}
+
+if (grid == NULL)
+{
+return (NULL);
+}
+
+return (grid);
+}
+
