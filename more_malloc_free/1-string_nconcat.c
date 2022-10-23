bc3274e46a0f3605c8469037d0de5e31635bdd51
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-22 19:24:29 -0700

    task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * string_nconcat - Short description
+ *
+ * @s1: first member
+ * @s2: second member
+ * @n: third member
+ * Return: Always 0 (Success)
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+char *s3;
+
+s3 = malloc(sizeof(char) * n);
+
+if (s3 == NULL)
+{
+return (NULL);
+}
+
+strcat(s3, s1);
+strcat(s3, s2);
+
+return (s3);
+}
+
