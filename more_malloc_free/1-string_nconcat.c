4e6b2b52f4e3c897481ef7927dca655408f698ab
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-23 13:43:26 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,8 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
 char *s3;
-unsigned int len2 = 0;
+unsigned int i, j, len1, len2;
 
 if (s1 == NULL)
 {
@@ -26,27 +25,32 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-
-while (s2[len2])
+for (i = 0; s1[i] != '\0'; i++)
+{
+len1++;
+}
+for (j = 0; s2[j] != '\0'; j++)
 {
 len2++;
-}
-
-if (n >= strlen(s2))
+if (n >= len2)
 {
-n = len2;
+n = s2[j];
 }
-
-s3 = malloc(sizeof(char) * (n + 1));
-
+}
+s3 = malloc(sizeof(char) * (len1 + n) +sizeof(char));
 if (s3 == NULL)
 {
 return (NULL);
 }
-
-strcat(s3, s1);
-strcat(s3, s2);
-
+for (i = 0; i < len1; i++)
+{
+s3[i] = s1[i];
+}
+for (j = 0; j < n; j++)
+{
+s3[i] = s2[j];
+}
+s3[i] = '\0';
 return (s3);
 }
 
