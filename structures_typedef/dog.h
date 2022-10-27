16b4d7a14969cf5c840d9874b3b786e3629f0adf
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-27 07:02:59 -0700

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Short description
+ * @name: name.
+ * @owner: owner.
+ * @age: age.
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+#endif
+
