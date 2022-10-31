9b49f370c2362744e6b8634e2c92db63f0dde0bb
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-31 13:30:00 -0700

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-	numbers += va_arg(ap, const unsigned int);
+	numbers = va_arg(ap, const unsigned int);
 	printf("%d", numbers);
 	if (separator == NULL)
 	{
