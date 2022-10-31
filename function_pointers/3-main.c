e46b2f07b08299b0f2833b9ca802e53f96888f45
toleman84 <gdponcemarsiglia@gmail.com>
2022-10-30 19:31:22 -0700

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Short decription.
+ * @argc: first member.
+ * @argv: second member.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	f = get_op_func(argv[2]);
+	num2 = atoi(argv[3]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3][0] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", f(num1, num2));
+
+	return (0);
+}
+
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Short description.
+ * @a: first member.
+ * @b: second member.
+ * Return: always 0 (Success)
+ */
+
+int op_add(int a, int b)
+	{
+		return (a + b);
+	}
+
+
+/**
+ * op_sub - Short description.
+ * @a: first member.
+ * @b: second member.
+ * Return: always 0 (Success)
+ */
+
+int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
+
+/**
+ * op_mul - Short description.
+ * @a: first member.
+ * @b: second member.
+ * Return: always 0 (Success)
+ */
+
+int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+
+/**
+ * op_div - Short description.
+ * @a: first member.
+ * @b: second member.
+ * Return: always 0 (Success)
+ */
+
+int op_div(int a, int b)
+	{
+		return (a / b);
+	}
+
+/**
+ * op_mod - Short description.
+ * @a: first member.
+ * @b: second member.
+ * Return: always 0 (Success)
+ */
+
+int op_mod(int a, int b)
+	{
+		return (a % b);
+	}
+
