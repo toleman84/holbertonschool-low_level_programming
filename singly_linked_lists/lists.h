8998cb5e22bf93e168068f132b2304f3de8af8a3
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-14 18:23:03 -0800

    task 1

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
-#ifndef _PRINT_LISTS_
-#define _PRINT_LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +24,9 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
