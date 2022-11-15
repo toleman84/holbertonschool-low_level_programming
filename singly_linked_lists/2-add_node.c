12b13a57175b9accc3be474978ef938afe56cc98
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-15 04:40:16 -0800

    task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - Short description.
+ * @str: first member.
+ *
+ * Return: new_node.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
+
