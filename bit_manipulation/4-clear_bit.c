10b3d3fd7148d2b5641af3e5a02eb8dcbdd07a1f
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-18 20:04:29 -0800

    task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -5,13 +5,17 @@
  * @n: first member.
  * @index: second member.
  *
- *
+ * Return: Always.
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
+	*n &= ~(1 << index);
 
+	return (1);
 }
 
