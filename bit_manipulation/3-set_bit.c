305d5babe0671eba33e78a09f1bc282bf6b69cd0
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-18 19:58:39 -0800

    task 3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -5,12 +5,17 @@
  * @n: first member.
  * @index: second member.
  *
- *
+ * Return: Always.
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
+	*n ^= (1 << index);
+
+	return (1);
 }
 
