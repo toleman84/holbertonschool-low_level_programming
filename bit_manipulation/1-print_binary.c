b03630deed8e74aeeb413c85d165d77cba000b35
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-18 19:35:59 -0800

    task 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,30 +4,14 @@
  * print_binary - Short desciption.
  * @n: first member.
  *
- *
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned int i = 0, num_max = 32768;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (num_max)
-	{
-		if (i == 1 && (n & num_max) == 0)
-			_putchar('0');
-		else if ((n & num_max) != 0)
-		{
-			_putchar('1');
-			i = 1;
-		}
-		i >>= 1;
-	}
-
+	_putchar((n & 1) + '0');
 }
 
