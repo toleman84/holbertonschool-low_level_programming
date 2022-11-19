e630e6cf444654d35b3c218976b202aba6146b4f
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-18 19:43:22 -0800

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * get_bit - Short description.
@@ -10,8 +10,16 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int max = 0x01;
 
+	max <<= index;
+	if (max == 0)
+		return (-1);
 
+	if ((n & max))
+		return (1);
+	else
+		return (0);
 
 }
 
