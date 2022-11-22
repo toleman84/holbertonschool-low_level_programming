d64e903e9827298f4da61ebe3f2a7f68d0961f6f
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-21 19:16:54 -0800

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,11 +3,13 @@
 /**
  * __exit - Print error messages
  * @error: numbers of errors.
+ * @s: is a name of files.
+ * @file: file descriptor.
  *
  * Return: 0 (Success)
  */
 
-int __exit(int error)
+int __exit(int error, char *s, int file)
 {
 	switch (error)
 	{
@@ -15,13 +17,13 @@ int __exit(int error)
 			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 			exit(error);
 		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
 			exit(error);
 		case 99:
-			dprintf(STDERR_FILENO, "Can't write to NAME_OF_THE_FILE\n");
+			dprintf(STDERR_FILENO, "Can't write to %s\n", s);
 			exit(error);
 		case 100:
-			dprintf(STDERR_FILENO, "Can't close fd FD_VALUE\n");
+			dprintf(STDERR_FILENO, "Can't close fd %d\n", file);
 			exit(error);
 		default:
 			return (0);
@@ -40,43 +42,43 @@ int __exit(int error)
 
 int main(int argc, char *argv[])
 {
-	int file1, file2, _read, _write;
+	int file_1, file_2, _read, _write;
 	char *buffer[1024];
 
 	if (argc != 3)
-		__exit(97);
+		__exit(97, NULL, 0);
 
 /*sets file descript    or copy to file*/
-	file2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file2 == -1)
-		__exit(99);
+	file_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_2 == -1)
+		__exit(99, argv[2], 0);
 
 /*sets file descriptor copy from file*/
-	file1 = open(argv[1], O_RDONLY);
-	if (file1 == -1)
-		__exit(98);
+	file_1 = open(argv[1], O_RDONLY);
+	if (file_1 == -1)
+		__exit(98, argv[1], 0);
 
 /*reads orginal file if long are more than 0 to read*/
 /*copies/writes content int new file*/
-	while ((_read = read(file1, buffer, 1024)) != 0)
+	while ((_read = read(file_1, buffer, 1024)) != 0)
 	{
 		if (_read == -1)
-			__exit(98);
+			__exit(98, argv[1], 0);
 
-		_write = write(file2, buffer, _read);
+		_write = write(file_2, buffer, _read);
 		if (_write == -1)
-			__exit(99);
+			__exit(99, argv[2], 0);
 	}
 
-	if (file2 == -1)
+	if (file_2 == -1)
 	{
-		__exit(100);
-		close(file2);
+		__exit(100, NULL, file_2);
+		close(file_2);
 	}
-	if (file1 == -1)
+	if (file_1 == -1)
 	{
-		__exit(100);
-		close(file1);
+		__exit(100, NULL, file_1);
+		close(file_1);
 	}
 
 	return (0);
