be7429fa25d522278d4a9fbfca651afdeb3ac560
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-27 12:48:33 -0800

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -12,6 +12,9 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	unsigned char comma_flag = 0;
 
+	if (ht == NULL)
+		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
