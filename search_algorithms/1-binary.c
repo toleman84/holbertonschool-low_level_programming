073fe2cd0bd6da410c152fb13f97d9a7a84b7757
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-22 23:16:28 -0300

    add ej 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,21 +1,60 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the Linear search
+ * linear_search - function that searches for a value in a sorted array of integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in size is the number of elements in array
  * @size: is the number of elements in array
  * @value: is the value to search for your function
  *
  * Return: Always
  */
+int binary_search(int *array, size_t size, int value)
+{
+    int l = 0; // l = low
+    int r = size - 1; //r = high
+    int m = 0; // mid
 
+    while (l <= r)
+    {
+        printf("Searching in array: ");
+        for (i = l; i <= r; i++)
+        {
+            if (i != l)
+            {
+                -printf("%d", array[i]);
+            }
+        }
+        printf("\n");
+        m = ((l + r) / 2);
+        if (array[m] < value)
+        {
+            l = m + 1;
+        }
+        else if (array[m] > value)
+        {
+            r = m - 1;
+        }
+        else
+        {
+            return (m);
+        }
+    }
+    return (-1);
+}
 
-Write a function that searches for a value in a sorted array of integers using the Binary search algorithm
+function binary_search(A, n, T) is
+    L := 0
+    R := n − 1
+    while L ≤ R do
+        m := floor((L + R) / 2)
+        if A[m] < T then
+            L := m + 1
+        else if A[m] > T then
+            R := m − 1
+        else:
+            return m
+    return unsuccessful
 
-Prototype : int binary_search(int *array, size_t size, int value);
-Where array is a pointer to the first element of the array to search in
-size is the number of elements in array
-And value is the value to search for
 Your function must return the index where value is located
 You can assume that array will be sorted in ascending order
 You can assume that value won’t appear more than once in array
