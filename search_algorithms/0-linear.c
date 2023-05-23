f169098e65ecf4f00fe1ea7498bd07557f5cb7c5
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-22 22:51:30 -0300

    fix ej 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -17,11 +17,11 @@ int linear_search(int *array, size_t size, int value)
 
     for (i = 0; i < array[size - 1]; i++)
     {
+        printf("Value checked array[%d] = [%d]\n", i, array[i]);
         if (array[i] == value)
         {
             return(i);
         }
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
     }
     return (-1);
 }
